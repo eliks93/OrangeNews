67adc919869994333d24a532b5fe988d33d4f1f8
Eli Kittross-Schnell <eliks92@gmail.com>
2020-01-31 22:33:55 -0800

    got cbc working, additionally fixed the issue with headless browsers never closing

--- orange-news-client/src/components/Articles.js
+++ orange-news-client/src/components/Articles.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 import Article from './Article'
+
 class Articles extends Component {
 constructor(props) {
     super(props);
--- python-scraper/scraper.py
+++ python-scraper/scraper.py
@@ -3,6 +3,12 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+
 
 def create_article(data):
   URL = "http://localhost:3001/articles"
@@ -10,14 +16,16 @@ def create_article(data):
   response = r.json()
   print(response)
 
-# CHROME_PATH = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
 WINDOW_SIZE = "1920,1080"
 
-chrome_options = Options()  
+chrome_options = Options()
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'    
+chrome_options.add_argument('user-agent={0}'.format(user_agent))
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--allow-running-insecure-content')
 chrome_options.add_argument("--headless")  
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-# chrome_options.binary_location = CHROME_PATH
 
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
                           options=chrome_options
@@ -44,7 +52,9 @@ def get_bbc():
     'link': link,
     'publisher': 'The BBC'
   }
+
   create_article(article)
+  print('create BBC article')
   
 def get_new_york():
   driver.get("https://www.nytimes.com/")
@@ -67,8 +77,40 @@ def get_new_york():
     'link': link,
     'publisher': 'The New York Times'
   }
+
   create_article(article)
-  print(snippets)
+  print('create NYT article')
+
+def get_cbc():
+  wait = WebDriverWait(driver, 8)
+  driver.get("https://www.cbc.ca/news")
+  content = driver.find_element_by_class_name('headline')
+  content.click()
+
+  wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'detailHeadline')))
+  soup = BeautifulSoup(driver.page_source)
+
+  headline = soup.find('h1').text
+  image = soup.find('img')['src']
+  snippet_one = soup.find('p')
+  snippet_two = snippet_one.find_next('p')
+  snippet_three = snippet_two.find_next('p')
+  snippets = snippet_one.text + '\n' + snippet_two.text + '\n' + snippet_three.text
+  link = driver.current_url
+  article = {
+    'headline': headline,
+    'snippet': snippets,
+    'image': image,
+    'link': link,
+    'publisher': 'The CBC'
+  }
+  create_article(article)
+  print('create CBC article')
+  
+
+
 
 get_new_york()
 get_bbc()
+get_cbc()
+driver.close()
